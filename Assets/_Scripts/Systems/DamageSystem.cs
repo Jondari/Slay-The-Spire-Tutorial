b2c689545f37f1098d1892b76a2baf101b4cbfef
Jondari <liaison.giovanni@laposte.net>
2026-09-15 16:42:24 +0200

    feat(combat): add enemy attack damage flow (tutorial part 14)
    Add AttackHeroGA and DealDamageGA game actions.
    
    Queue enemy attacks during enemy turns and animate attackers with DOTween.
    
    Add DamageSystem to apply damage, update combatant health, shake targets, and spawn hit VFX.
    
    Import Cartoon FX hit VFX assets used by the damage system.

---
+++ Assets/_Scripts/Systems/DamageSystem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using UnityEngine;
+
+public class DamageSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject damageVFX;
+
+    private void OnEnable()
+    {
+        ActionSystem.AttachPerformer<DealDamageGA>(DealDamagePerformer);
+    }
+
+    private void OnDisable()
+    {
+        ActionSystem.DetachPerformer<DealDamageGA>();
+    }
+
+    private IEnumerator DealDamagePerformer(DealDamageGA dealDamageGA)
+    {
+
+        foreach (var target in dealDamageGA.Targets)
+        {
+            target.Damage(dealDamageGA.Amount);
+            //Intanciante the VFX
+            Instantiate(damageVFX, target.transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(0.15f);
+        }
+   
+    }
+}
--- Assets/_Scripts/Systems/EnemySystem.cs
+++ Assets/_Scripts/Systems/EnemySystem.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,11 +9,13 @@ public class EnemySystem : Singleton<EnemySystem>
     private void OnEnable()
     {
         ActionSystem.AttachPerformer<EnemyTurnGA>(EnemyTurnPerformer);
+        ActionSystem.AttachPerformer<AttackHeroGA>(AttackHeroPerformer);
     }
 
     private void OnDisable()
     {
         ActionSystem.DetachPerformer<EnemyTurnGA>();
+        ActionSystem.DetachPerformer<AttackHeroGA>();
     }
 
     public void Setup(List<EnemyData> enemyDatas)
@@ -28,7 +31,24 @@ public class EnemySystem : Singleton<EnemySystem>
     private IEnumerator EnemyTurnPerformer(EnemyTurnGA enemyTurnGA)
     {
         Debug.Log("Enemy Turn");
-        yield return new WaitForSeconds(2f);
+        foreach (var enemy in enemyBoardView.EnemyViews)
+        {
+            AttackHeroGA attackHeroGA = new(enemy);
+            ActionSystem.Instance.AddReaction(attackHeroGA);
+        }
         Debug.Log("End Enemy Turn");
+        yield return null;
+    }
+
+    private IEnumerator AttackHeroPerformer(AttackHeroGA attackHeroGA)
+    {
+        EnemyView attacker = attackHeroGA.Attacker;
+        // Attack animation (this is a simple example)
+        Tween tween = attacker.transform.DOMoveX(attacker.transform.position.x - 1f, 0.15f);
+        yield return tween.WaitForCompletion();
+        attacker.transform.DOMoveX(attacker.transform.position.x + 1f, 0.15f);
+        // Deal Damage
+        DealDamageGA dealDamageGA = new(attacker.AttackPower, new() { HeroSystem.Instance.HeroView });
+        ActionSystem.Instance.AddReaction(dealDamageGA);
     }
 }
