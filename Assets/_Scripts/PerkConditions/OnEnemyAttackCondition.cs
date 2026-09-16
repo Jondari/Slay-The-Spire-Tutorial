c3c9ccd0cdb07a526b51b15e95fef8d37275bcac
Jondari <liaison.giovanni@laposte.net>
2026-09-16 15:03:31 +0200

    feat(perks): add counterattack perk data (tutorial part 19)
    Add perk data and condition models.
    
    Add an enemy attack condition for reacting to AttackHeroGA.
    
    Create a Counterattack perk that deals damage back to the attacker.

---
+++ Assets/_Scripts/PerkConditions/OnEnemyAttackCondition.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+public class OnEnemyAttackCondition : PerkCondition
+{
+    public override bool SubConditionIsMet(GameAction gameAction)
+    {
+        // the condition is met because we want to react to all attacker.
+        return true;
+    }
+
+    public override void SubscribeCondition(Action<GameAction> reaction)
+    {
+        ActionSystem.SubscribeReaction<AttackHeroGA>(reaction, reactionTiming);
+    }
+
+    public override void UnsubscribeCondition(Action<GameAction> reaction)
+    {
+        ActionSystem.UnsubscribeReaction<AttackHeroGA>(reaction, reactionTiming);
+    }
+}
