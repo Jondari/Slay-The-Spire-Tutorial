4e7b7a8c05e110b5f285d1881208fbb0e4c1b080
Jondari <liaison.giovanni@laposte.net>
2026-09-18 12:57:43 +0200

    feat(status-effects): add combatant status effect UI (tutorial part 22)
    Add stack-aware UI components for armor and burn effects.
    
    Attach the status effect display to the shared combatant prefab.
    
    Include status icons and the supporting effect type enum.

---
+++ Assets/_Scripts/UI/StatusEffectsUI.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class StatusEffectsUI : MonoBehaviour
+{
+    [SerializeField] private StatusEffectUI statusEffectUIPrefab;
+    [SerializeField] private Sprite armorSprite, burnSprite;
+    private Dictionary<StatusEffectType, StatusEffectUI> statusEffectUIs = new();
+
+    public void UpdateStatusEffectUI(StatusEffectType statusEffectType, int stackCount)
+    {
+        // we remove the ui if there is one
+        if (stackCount == 0)
+        {
+            if (statusEffectUIs.ContainsKey(statusEffectType))
+            {
+                StatusEffectUI statusEffectUI = statusEffectUIs[statusEffectType];
+                statusEffectUIs.Remove(statusEffectType);
+                Destroy(statusEffectUI.gameObject);
+            }
+        }
+        // if the stack count is not 0 we create the UI
+        else 
+        {
+            if (!statusEffectUIs.ContainsKey(statusEffectType))
+            {
+                StatusEffectUI statusEffectUI = Instantiate(statusEffectUIPrefab, transform);
+                statusEffectUIs.Add(statusEffectType, statusEffectUI);
+            }
+            Sprite sprite = GetSpriteByType(statusEffectType);
+            statusEffectUIs[statusEffectType].Set(sprite, stackCount);
+        }
+    }
+
+    private Sprite GetSpriteByType(StatusEffectType statusEffectType)
+    {
+        return statusEffectType switch
+        {
+            StatusEffectType.ARMOR => armorSprite,
+            StatusEffectType.BURN => burnSprite,
+            _ => null,
+        };
+    }
+}
