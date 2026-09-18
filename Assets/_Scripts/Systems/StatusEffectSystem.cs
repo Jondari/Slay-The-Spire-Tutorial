dfbcad786b5090ffca1442828e933afb29767574
Jondari <liaison.giovanni@laposte.net>
2026-09-18 14:29:36 +0200

    feat(status-effects): implement armor mechanics (tutorial part 23)
    Add stackable status application and hero targeting.
    
    Consume armor before health when resolving incoming damage.
    
    Add Armor Up and include it in the default hero deck.

---
+++ Assets/_Scripts/Systems/StatusEffectSystem.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class StatusEffectSystem : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        ActionSystem.AttachPerformer<AddStatusEffectGA>(AddStatusEffectPerformer);
+    }
+
+    private void OnDisable()
+    {
+        ActionSystem.DetachPerformer<AddStatusEffectGA>();
+    }
+
+    private IEnumerator AddStatusEffectPerformer(AddStatusEffectGA addStatusEffectGA)
+    {
+        foreach(var target in addStatusEffectGA.Targets)
+        {
+            target.AddStatusEffect(addStatusEffectGA.StatusEffectType, addStatusEffectGA.StackCount);
+            yield return null; // TODO ADD VFX for adding status effects
+        }
+    }
+}
